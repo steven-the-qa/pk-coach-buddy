5384d6edd98cf6efe578cfe771c3ce594998134c
Steven Boutcher <boutchersj@gmail.com>
2025-02-28 12:17:56 -0600

    Add Supabase

---
+++ supabase.ts
@@ -0,0 +1,6 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ovmsheguhtvhomizosbs.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey as string)
